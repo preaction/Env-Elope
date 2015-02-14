012137b4a106b532505385221fa0f0fddaf1815b
Doug Bell <madcityzen@gmail.com>
2015-02-14 14:00:06 -0600

    add initial Env::Elope config module

--- lib/Env/Elope.pm
+++ lib/Env/Elope.pm
@@ -1,15 +1,44 @@
 package Env::Elope;
-# ABSTRACT: Write a sentence about what it does
+# ABSTRACT: Configure projects in multiple environments
 
 use strict;
 use warnings;
+use Sub::Exporter -setup => {
+    exports => [qw( dist_env_dir )],
+};
+use File::Share qw( dist_dir );
+use Path::Tiny qw( path );
 
+=sub dist_env_dir DIST, OPTIONS
 
+Get the configuration directory for the given C<DIST>. Returns a L<Path::Tiny>
+object with the given dist path.
+
+Configuration directories are located in a distribution's C<share/env/> directory.
+Which directory is chosen is based on the C<ELOPE_ENV> environment variable.
+
+C<OPTIONS> is a set of key/value pairs for options with the following keys:
+
+    env     - Set the environment name, overriding the current environment
+
+=cut
+
+sub dist_env_dir($%) {
+    my ( $dist, %opt ) = @_;
+    my $env = $opt{env} || $ENV{ELOPE_ENV} || die "ELOPE_ENV not set!";
+    return path( dist_dir( $dist ), 'env', $env );
+}
 
 1;
 __END__
 
 =head1 SYNOPSIS
 
+    use Env::Elope qw( dist_env_dir );
+
+    my $config = dist_env_dir( 'My-Project' )->child( 'config.yml' );
+
 =head1 DESCRIPTION
 
+This module manages configuration directories based on the what environment
+the code is running in.
